a2ae6c54f6c54f2bcca1f31ba93ce46dd6329945
jnunez <jnunez16@alumnos.utalca.cl>
2023-10-10 01:50:10 -0300

    arreglosssssssss

--- src/T1.java
+++ src/T1.java
@@ -4,43 +4,37 @@ public class T1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
-    if (args.length > 0) {
-        // Obtener el primer argumento proporcionado en la lÃ­nea de comandos
-        String primerArgumento = args[0];
-        
-        Scanner sc = new Scanner(System.in);
-        String expresion=sc.nextLine();
+        if (args.length > 0) {
+            // Obtener el primer argumento proporcionado en la lÃ­nea de comandos
+            String expresion = args[0];
 
-        Simulador simulador = new Simulador();
+            Convertidor convertidor = new Convertidor();
+            expresion = convertidor.conversor(expresion);
 
-        Convertidor convertidor = new Convertidor();
-        expresion = convertidor.conversor(expresion);
+            AFND algoritmoThomson = new AFND(expresion);
 
-        AFND algoritmoThomson = new AFND(expresion);
-        
-        algoritmoThomson.construirAFND();
-        //AFND resultante
-        algoritmoThomson.agregarSignoAlfabetoGeneral();
-        Automata resultado_afnd = algoritmoThomson.getAfnd();
+            algoritmoThomson.construirAFND();
+            // AFND resultante
+            algoritmoThomson.agregarSignoAlfabetoGeneral();
+            Automata resultado_afnd = algoritmoThomson.getAfnd();
 
-        System.out.println(resultado_afnd);
+            System.out.println(resultado_afnd);
 
-        AFD afd= new AFD();
+            AFD afd = new AFD();
 
-        afd.convertirAFND_AFD(resultado_afnd);
-        afd.agregarSignoAlfabetoGeneral();
+            afd.convertirAFND_AFD(resultado_afnd);
+            afd.agregarSignoAlfabetoGeneral();
 
-        Automata resultado_afd = afd.getAfd();        
-        System.out.println(resultado_afd);
-        
-        // Resto de tu cÃ³digo...
-    } else {
-        // Si no se proporcionan argumentos, imprimir un mensaje de error
-        System.out.println("No se proporcionaron argumentos.");
+            Automata resultado_afd = afd.getAfd();
+            System.out.println(resultado_afd);
+
+            // Resto de tu cÃ³digo...
+        } else {
+            // Si no se proporcionan argumentos, imprimir un mensaje de error
+            System.out.println("No se proporcionaron argumentos.");
+        }
     }
-    }
-    
+
 }
